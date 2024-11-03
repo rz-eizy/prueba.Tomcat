9afa0901f74b7a2018ab296586597094d1ed5199
rz-eizy <duartealessandroxd@gmail.com>
2024-11-03 00:37:05 -0300

    Primer Commit

---
+++ src/main/java/org/example/testtom/HelloServlet.java
@@ -0,0 +1,22 @@
+package org.example.testtom;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    public void init() {
+        message = "Test de Tomcat: ¡Funciona!";
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+    public void destroy() { }
+}
+
